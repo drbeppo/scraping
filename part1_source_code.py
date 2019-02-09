035da6322ee7cf3f784d8bb33139f198fe14b317
DrBeppo <47199833+drbeppo@users.noreply.github.com>
2019-02-09 18:43:54 +0900

    Add files via upload

---
+++ part1_source_code.py
@@ -0,0 +1,39 @@
+#coding:utf-8
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+
+browser = webdriver.Firefox()
+browser.get('ここにURLを入力する')
+
+names_xpath = "//*[@id=\"mw-content-text\"]/div/table[5]/tbody/tr[*]/td[1]/a"
+abstract_xpath ="/html/body/div[3]/div[3]/div[4]/div/p[1]"
+
+idol_elements = browser.find_elements(By.XPATH, names_xpath)
+
+for idol_element in idol_elements:
+    name = idol_element.text
+    print(name)
+sleep(3)
+idol_element.click()
+abstract_element = browser.find_element(By.XPATH, abstract_xpath)
+abstract = abstract_element.text
+print(abstract)
+
+
+
+
+
+
+
+
+
+
+
+
+
+#    browser.back()
+#//*[@id="mw-content-text"]/div/table[5]/tbody/tr[1]/td[1]/a
+#//*[@id="mw-content-text"]/div/table[5]/tbody/tr[2]/td[1]/a
+#/html/body/div[3]/div[3]/div[4]/div/p[1]
+#/html/body/div[3]/div[3]/div[4]/div/p[1]
